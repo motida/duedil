23a51f1dc34bf5f6e05198f79f85ed272222ab3a
motida2 <motida2@gmail.com>
2019-12-11 21:11:50 +0200

    further scaling todos use lru in provide match method

--- match.py
+++ match.py
@@ -1,3 +1,7 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=100)
 def match(name, companies, min_threshold):
     """
     :param name: name provided in the user uploaded record.
@@ -7,15 +11,17 @@ def match(name, companies, min_threshold):
 
     :return: dictionary with the best match, fields are `match_name`, `match_id` and `score`.
     """
+    name_upper = name.upper()
+    record_name_tokens = set(name_upper.split())
+
     best_match = None
     best_score = 0
     for company in companies:
         # Unify casing for both names
-        name_upper = name.upper()
+
         company_name_upper = company['name'].upper()
 
         # Extract tokens (words) from names
-        record_name_tokens = set(name_upper.split())
         company_name_tokens = set(company_name_upper.split())
 
         # measure name similarity using Jaccard coefficient measured as
